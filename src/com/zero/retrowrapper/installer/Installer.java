2f380715bb4d90c798cafe114b85777b3d90ba7b
LOYND Ned <47479@2020-47479.local>
2020-04-05 02:19:22 +1100

    More flexible installer, made codebase worse
    Seperated functionality out a bit with methods to allow easier re-use of functions in the future, made the installer not close after any action, and made the installer refresh after a version is wrapper or all Retrowrapper versions are uninstalled. The codebase is now a mess, and uses a ton of bad practices. I'll tidy it up at some point.

--- src/com/zero/retrowrapper/installer/Installer.java
+++ src/com/zero/retrowrapper/installer/Installer.java
@@ -23,46 +23,155 @@ import javax.swing.JComboBox;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JOptionPane;
+import javax.swing.MutableComboBoxModel;
 import javax.swing.SwingConstants;
 import javax.swing.UIManager;
 import javax.swing.WindowConstants;
 
 public class Installer
 {
+	
+	//I know that this is horrible code and practices. It will work for awhile, but when it breaks this message will make me or you despair that I didn't do a better job.
+	
+	int versionCount;
+	String workingDirectory;
+	File directory;
+	File[] directories;
+	File versions;
+	JButton install;
+	JButton uninstall;
+	
+	JComboBox<String> list = new JComboBox<String>();
+	MutableComboBoxModel<String> model = (MutableComboBoxModel<String>)list.getModel(); //a surprise to be used later
+	
+    String installerVersion = "1.4.2";
+    String bonusText = "Now with more wrapping!"; 
+	
+	public String defaultWorkingDirectory() {
+		
+		String OS = (System.getProperty("os.name")).toUpperCase();
+		
+		String defaultWorkingDirectory;
+		
+		if (OS.contains("WIN")) //windows uses the %appdata%/.minecraft structure
+		{
+			defaultWorkingDirectory = System.getenv("AppData") + File.separator + ".minecraft";
+		}
+		else if (OS.contains("MAC")) //mac os uses %user%/Library/Library/Application Support/minecraft
+		{
+			defaultWorkingDirectory = System.getProperty("user.home") + File.separator + "Library" + File.separator + "Application Support" + File.separator + "minecraft";
+		}else 
+		{
+			defaultWorkingDirectory = System.getProperty("user.home") + File.separator + ".minecraft";
+		} 
+		
+		return defaultWorkingDirectory;
+
+	}
+	
+	void refreshList(String givenDirectory) {
+		
+		versionCount = 0;
+		list.removeAllItems();
+		
+		if (!givenDirectory.isEmpty()) {
+			directory = new File(givenDirectory);
+			
+			directory.mkdirs();
+		
+			directories = null;
+			versions = new File(directory, "versions");
+		
+			if(versions.exists())
+			{
+				directories = versions.listFiles();
+			}
+			
+			if(!(directories == null || directories.length == 0)) {
+		
+			Arrays.sort(directories);
+		
+			//add items
+		
+			for(File f : directories)
+			{
+				if(f.isDirectory())
+			{
+				versionCount++;
+				File json = new File(f, f.getName() + ".json");
+				File jar = new File(f, f.getName() + ".jar");
+				if(json.exists() && jar.exists() && !f.getName().contains("-wrapped") && !new File(versions, f.getName()+"-wrapped").exists())
+				{
+					try(Scanner s = new Scanner(json).useDelimiter("\\A"))
+					{
+						String content = s.next();
+						if (content.contains("old_") && !content.contains("retrowrapper")) {
+							model.addElement(f.getName());
+						}
+						} catch (FileNotFoundException e) {
+							// TODO Auto-generated catch block
+							e.printStackTrace();
+						}
+					}
+				}
+			}
+		
+		}
+		}
+		
+		//button visibility
+		
+		if (givenDirectory.isEmpty()) {
+		
+			install.setEnabled(false);
+			uninstall.setEnabled(false);
+			
+		JOptionPane.showMessageDialog(null, "No directory / minecraft directory detected!\n", "Error", JOptionPane.INFORMATION_MESSAGE);
+		
+		} else if(!versions.exists())
+		{
+			install.setEnabled(false);
+			uninstall.setEnabled(false);
+			
+			JOptionPane.showMessageDialog(null, "No Minecraft versions folder found!", "Error", JOptionPane.INFORMATION_MESSAGE);
+			
+		} else if (versionCount == 0) {
+			
+			install.setEnabled(false);
+			uninstall.setEnabled(true);
+			
+			JOptionPane.showMessageDialog(null, "No wrappable versions found!", "Error", JOptionPane.INFORMATION_MESSAGE);
+			
+		} else if (list.getItemCount() == 0) {
+			
+			install.setEnabled(false);
+			uninstall.setEnabled(true);
+			
+			JOptionPane.showMessageDialog(null, "All detected versions have already been wrapped!", "Info", JOptionPane.INFORMATION_MESSAGE);
+			
+		} else {
+			
+			install.setEnabled(true);
+			uninstall.setEnabled(true);
+			
+		}
+		
+	}
+	
 	public Installer() throws Exception
 	{
-	    String installerVersion = "1.4.1";
-	    String bonusText = "Now with Mac support!";
 		
-		String workingDirectory = "";
-		String workingDirectoryName = "";
-		String OS = (System.getProperty("os.name")).toUpperCase();
-		if (OS.contains("WIN"))
-		{
-		    workingDirectory = System.getenv("AppData");
-		    workingDirectoryName = ".minecraft";
-		}
-		else if (OS.contains("OS X"))
-		{
-		    workingDirectory = System.getProperty("user.home") + "/Library/Application Support";
-		    workingDirectoryName = "minecraft";
-		}else
-		{
-			workingDirectory = System.getProperty("user.home");
-			workingDirectoryName = ".minecraft";
-		}
-
-		workingDirectory = workingDirectory + File.separator + workingDirectoryName + File.separator;
-		final File directory = new File(workingDirectory);
-		directory.mkdirs();
+		workingDirectory = defaultWorkingDirectory();
+		
+		//final File[] directoriesFinal = directories;
 		
 		UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
-
+		
 		JFrame frame = new JFrame("Retrowrapper Unofficial Continuation");
 		frame.setPreferredSize(new Dimension(654, 310));
 		frame.setLayout(null);
 		frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
-		frame.setResizable(false);
+		frame.setResizable(false); //add resizing later
 		
 		JLabel label = new JLabel("Retrowrapper installer");
 		label.setFont(label.getFont().deriveFont(20f).deriveFont(Font.BOLD));
@@ -82,56 +191,18 @@ public class Installer
 		label2.setBounds(0, 250, 654, 20);
 		frame.add(label2);
 		
-		final JComboBox<String> list = new JComboBox<String>();
 		list.setBounds(654/2 - 150, 100, 300, 30);
 		
-		File[] directories = null;
-		final File versions = new File(directory, "versions");
-		if(versions.exists())
-		{
-			directories = versions.listFiles();
-		}
-		
-		if(directories == null || directories.length == 0)
-		{
-	    	JOptionPane.showMessageDialog(null, "No wrappable versions found!", "Error", JOptionPane.ERROR_MESSAGE);
-	    	System.exit(0);			
-		}
-		
-		Arrays.sort(directories);
-		
-		final File[] directoriesFinal = directories;
-		
-		for(File f : directoriesFinal)
-		{
-			if(f.isDirectory())
-			{
-				File json = new File(f, f.getName() + ".json");
-				File jar = new File(f, f.getName() + ".jar");
-				if(json.exists() && jar.exists() && !f.getName().contains("-wrapped") && !new File(versions, f.getName()+"-wrapped").exists())
-				{
-					try(Scanner s = new Scanner(json).useDelimiter("\\A"))
-					{
-						String content = s.next();
-						if(content.contains("old_") && !content.contains("retrowrapper"))
-						{
-							list.addItem(f.getName());
-						}
-					}
-				}
-			}
-		}
-
 		frame.add(list);
 		
-		JButton uninstall = new JButton("Uninstall");
+		uninstall = new JButton("Uninstall all"); //uninstaller code
 		uninstall.setBounds(654/2 - 100, 202, 200, 30);
 		uninstall.addActionListener(new ActionListener()
 		{
 		    @Override
 		    public void actionPerformed(ActionEvent e)
 		    {
-		    	for(File f : directoriesFinal)
+		    	for(File f : directories)
 				{
 					if(f.isDirectory() && f.getName().contains("-wrapped"))
 					{
@@ -146,7 +217,8 @@ public class Installer
 		    	}
 
 		    	JOptionPane.showMessageDialog(null, "Successfully uninstalled wrapper", "Success", JOptionPane.INFORMATION_MESSAGE);
-		    	System.exit(0);
+		    	refreshList(workingDirectory);
+		    	//System.exit(0);
 		    }
 
 			private void deleteDirectory(File f)
@@ -174,11 +246,12 @@ public class Installer
 					ee.printStackTrace();
 				}				
 			}
+			
 		});
 		
 		frame.add(uninstall);
 
-		JButton install = new JButton("Install");
+		install = new JButton("Install"); //installation code
 		install.setBounds(654/2 - 100, 160, 200, 40);
 		
 		install.addActionListener(new ActionListener()
@@ -227,7 +300,8 @@ public class Installer
 				}		    	
 		    	
 		    	JOptionPane.showMessageDialog(null, "Successfully wrapped version\n"+version, "Success", JOptionPane.INFORMATION_MESSAGE);
-		    	System.exit(0);
+		    	refreshList(workingDirectory);
+		    	//System.exit(0);
 		    }
 		});
 		
@@ -236,6 +310,8 @@ public class Installer
 		frame.pack();
 		frame.setVisible(true);
 		frame.setLocationRelativeTo(null);
+		
+		refreshList(workingDirectory);
 	}
 	
 	public static void main(String[] args)
@@ -246,7 +322,7 @@ public class Installer
 		}catch(Exception e)
 		{
 			e.printStackTrace();
-			JOptionPane.showMessageDialog(null, "Exception occured!\n"+e.getMessage());
+			JOptionPane.showMessageDialog(null, "Exception occured!\n"+e.getMessage(), "Error", JOptionPane.ERROR_MESSAGE);
 		}
 	}
 }
